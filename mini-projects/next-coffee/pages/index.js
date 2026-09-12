3a6b506ba0d3982b907d9003c06497d3f558eca3
Alireza <andromedaa5692@gmail.com>
2026-09-12 20:21:40 +0330

    Develop Services Template in HomePage

--- mini-projects/next-coffee/pages/index.js
+++ mini-projects/next-coffee/pages/index.js
@@ -1,12 +1,14 @@
 import About from "@/components/templates/Index/About";
 import Slider from "@/components/templates/Index/Slider";
 import React from "react";
+import Services from "@/components/templates/Index/Services";
 
 function Home() {
   return (
     <>
       <Slider />
       <About />
+      <Services />
     </>
   );
 }
