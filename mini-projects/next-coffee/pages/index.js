a05b9f6cc3bc77ee3d126fde99c465dd57710ad6
Alireza <andromedaa5692@gmail.com>
2026-09-12 20:09:17 +0330

    develop About Template for homePage - templates/Index/About

--- mini-projects/next-coffee/pages/index.js
+++ mini-projects/next-coffee/pages/index.js
@@ -1,3 +1,4 @@
+import About from "@/components/templates/Index/About";
 import Slider from "@/components/templates/Index/Slider";
 import React from "react";
 
@@ -5,6 +6,7 @@ function Home() {
   return (
     <>
       <Slider />
+      <About />
     </>
   );
 }
