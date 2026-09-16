33559dcf17e18d2bca7dacbf5d2ac3fb00c330a7
Alireza <andromedaa5692@gmail.com>
2026-09-16 21:20:48 +0330

    develop testimonial section in homePage

--- mini-projects/next-coffee/pages/index.js
+++ mini-projects/next-coffee/pages/index.js
@@ -5,6 +5,7 @@ import Services from "@/components/templates/Index/Services";
 import Offer from "@/components/templates/Index/Offer";
 import Menu from "@/components/templates/Index/Menu";
 import Reservation from "@/components/templates/Index/Reservation";
+import Testimonials from "@/components/templates/Index/Testimonials";
 
 function Home({ data }) {
   return (
@@ -15,6 +16,7 @@ function Home({ data }) {
       <Offer />
       <Menu data={data.menu} />
       <Reservation />
+      <Testimonials data={data.comments} />
     </>
   );
 }
@@ -25,11 +27,16 @@ export async function getStaticProps() {
 
   const menuReponse = await fetch("http://localhost:4000/menu");
   const menuData = await menuReponse.json();
+
+  const commentsResponse = await fetch("http://localhost:4000/comments");
+  const commentsData = await commentsResponse.json();
+
   return {
     props: {
       data: {
         services: servicesData,
         menu: menuData,
+        comments: commentsData,
       },
       revalidate: 60 * 60 * 12, // 12 Hours
     },
