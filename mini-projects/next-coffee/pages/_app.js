f49ad3cf2ef189e1ded673c637df0359846d2a23
Alireza <andromedaa5692@gmail.com>
2026-09-16 21:25:43 +0330

    develop Footer of Project

--- mini-projects/next-coffee/pages/_app.js
+++ mini-projects/next-coffee/pages/_app.js
@@ -1,3 +1,4 @@
+import Footer from "@/components/modules/Footer/Footer";
 import Navbar from "@/components/modules/Navbar/Navbar";
 import "@/styles/globals.css";
 
@@ -7,6 +8,7 @@ export default function App({ Component, pageProps }) {
       <Navbar />
 
       <Component {...pageProps} />
+      <Footer />
     </>
   );
 }
