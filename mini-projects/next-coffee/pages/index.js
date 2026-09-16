e56e0b590a5b3e1eaa62f55087140f69a4d8c374
Alireza <andromedaa5692@gmail.com>
2026-09-16 20:47:29 +0330

    develop menuCaffe in homePage

--- mini-projects/next-coffee/pages/index.js
+++ mini-projects/next-coffee/pages/index.js
@@ -3,6 +3,7 @@ import Slider from "@/components/templates/Index/Slider";
 import React from "react";
 import Services from "@/components/templates/Index/Services";
 import Offer from "@/components/templates/Index/Offer";
+import Menu from "@/components/templates/Index/Menu";
 
 function Home({ data }) {
   return (
@@ -11,19 +12,22 @@ function Home({ data }) {
       <About />
       <Services services={data.services} />
       <Offer />
+      <Menu data={data.menu} />
     </>
   );
 }
 
 export async function getStaticProps() {
-  const res = await fetch("http://localhost:4000/services");
+  const servicesResponse = await fetch("http://localhost:4000/services");
+  const servicesData = await servicesResponse.json();
 
-  const services = await res.json();
-
+  const menuReponse = await fetch("http://localhost:4000/menu");
+  const menuData = await menuReponse.json();
   return {
     props: {
       data: {
-        services,
+        services: servicesData,
+        menu: menuData,
       },
       revalidate: 60 * 60 * 12, // 12 Hours
     },
