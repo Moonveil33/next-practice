43f3c808e81497f241c1556ba7578a81baed175c
Alireza <andromedaa5692@gmail.com>
2026-09-21 21:29:46 +0330

    develop ProductsDetails Page -- SSG - use getStaticPath

--- mini-projects/next-coffee/pages/products/[id].js
+++ mini-projects/next-coffee/pages/products/[id].js
@@ -1,7 +1,41 @@
-import React from "react";
+import Comments from "@/components/templates/Product/Comments";
 
-function ProductDetails() {
-  return <div>ProductDetails</div>;
+import ProductsDetails from "@/components/templates/Product/ProductsDetails";
+
+const Product = ({ product }) => {
+  return (
+    <>
+      <ProductsDetails data={product} />
+      <Comments />
+    </>
+  );
+};
+
+export async function getStaticPaths(context) {
+  const res = await fetch(`http://localhost:4000/menu`);
+  const products = await res.json();
+
+  const paths = products.map((product) => ({
+    params: { id: String(product.id) },
+  }));
+
+  return {
+    paths,
+    fallback: false,
+  };
 }
 
-export default ProductDetails;
+export async function getStaticProps(context) {
+  const { params } = context;
+
+  const res = await fetch(`http://localhost:4000/menu/${params.id}`);
+  const product = await res.json();
+
+  return {
+    props: {
+      product,
+    },
+  };
+}
+
+export default Product;
