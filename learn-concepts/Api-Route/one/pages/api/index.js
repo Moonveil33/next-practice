59c46f3c876223d287db366c96ded66aca5b9e35
Alireza <andromedaa5692@gmail.com>
2026-09-24 14:24:59 +0330

    learn how to access to request method

--- learn-concepts/Api-Route/one/pages/api/index.js
+++ learn-concepts/Api-Route/one/pages/api/index.js
@@ -1,5 +1,31 @@
+import React from "react";
+
 function handler(req, res) {
-  return res.json({ message: "IndexPage" });
+  switch (req.method) {
+    case "GET": {
+      return res.json({ message: "Welcome To index Page" });
+      break;
+    }
+
+    case "POST": {
+      return res.json({ message: "user created successfully" });
+      break;
+    }
+
+    case "PUT": {
+      return res.json({ message: "user replace successfully" });
+    }
+
+    case "DELETE": {
+      res.json({ message: "user Removed Successfully" });
+    }
+
+    default: {
+      res.json({
+        message: "welcome",
+      });
+    }
+  }
 }
 
 export default handler;
