cc7c3933fd38a008b933a46cf9c9160cde3ddce4
Alireza <andromedaa5692@gmail.com>
2026-09-26 16:43:36 +0330

    learn how to develop PUT Api for edit user data

--- learn-concepts/Api-Route/one/pages/api/users/[id].js
+++ learn-concepts/Api-Route/one/pages/api/users/[id].js
@@ -43,6 +43,37 @@ const handler = (req, res) => {
     } else {
       return res.status(404).json({ message: "user not found" });
     }
+  } else if (req.method === "PUT") {
+    const { id } = req.query;
+    const { username, email, password } = req.body;
+
+    const dbPath = path.join(process.cwd(), "data", "db.json");
+    const data = fs.readFileSync(dbPath);
+    const parsedData = JSON.parse(data);
+    const isUser = parsedData.users.some(
+      (user) => String(user.id) === String(id),
+    );
+
+    if (isUser) {
+      parsedData.users.some((user) => {
+        if (String(user.id) === String(id)) {
+          user.username = username;
+          user.email = email;
+          user.password = password;
+          return true;
+        }
+      });
+
+      const err = fs.writeFileSync(dbPath, JSON.stringify(parsedData));
+
+      if (err) {
+        // Coding
+      } else {
+        return res.json({ message: "User Updated Successfully" });
+      }
+    } else {
+      return res.status(404).json({ message: "user not found" });
+    }
   }
 };
 
